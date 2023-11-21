f84419cba290f922acaaa51e0a975226f810047f
omyasar <omyasar68@gmail.com>
2023-11-21 20:25:54 +0100

    migrate_constructorresults functie toegevoegd

--- databasevullen.py
+++ databasevullen.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from db_conn import db_connection
 
 # Connectie naar mongodb en datacollectie
 # Wijzig indien nodig je database naam (Deze naam hebben we onderling besproken voor mongodb)
@@ -19,3 +20,36 @@ mongo_results = mongo_db['results']
 mongo_status = mongo_db['status']
 mongo_sprint_results = mongo_db['sprint_results']
 mongo_season_list = mongo_db['season_list']
+
+
+def migrate_constructorresults():
+    conn, cursor = db_connection()
+
+    # Tranformeer de data
+    transformed_data = []
+    for data in mongo_constructor_results.find():
+        transformed_data.append((
+            data['_id'],
+            data['constructorResultsId'],
+            data['raceId'],
+            data['constructorId'],
+            data['points'] if data['points'] != '\\N' else None,
+            data['status'] if data['status'] != '\\N' else None
+        ))
+    try:
+        cursor.executemany(
+            """
+            INSERT INTO constructor_results (
+                _id, constructoRresultsId, raceid, constructorid, points, status
+            )
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+            transformed_data
+        )
+        conn.commit()
+    except Exception as e:
+        print(f"Data migratie fout: {e}")
+        conn.rollback()
+
+
+
